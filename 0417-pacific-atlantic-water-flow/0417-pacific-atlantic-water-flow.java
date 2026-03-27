8e2c664396d229479cfe127d706845c17166c32d
Aleena Burney <aleenaburney17@gmail.com>
2026-03-27 15:11:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,69 +1,57 @@
-import java.util.*;
-
 class Solution {
-    int m, n;
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    int n, m;
+    int[][] grid;
+    int[][] pacific;
+    int[][] atlantic;
+
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
 
-        boolean[][] pac = new boolean[m][n];
-        boolean[][] atl = new boolean[m][n];
+        grid = heights;
+        n = heights.length;
+        m = heights[0].length;
 
-        Queue<int[]> pq = new LinkedList<>();
-        Queue<int[]> aq = new LinkedList<>();
+        pacific = new int[n][m];
+        atlantic = new int[n][m];
 
-        // Pacific borders
-        for (int i = 0; i < m; i++) {
-            pq.offer(new int[]{i, 0});
-            pac[i][0] = true;
-        }
-        for (int j = 0; j < n; j++) {
-            pq.offer(new int[]{0, j});
-            pac[0][j] = true;
-        }
+        // Pacific (top row + left column)
+        for (int i = 0; i < n; i++) dfs(i, 0, pacific);
+        for (int j = 0; j < m; j++) dfs(0, j, pacific);
 
-        // Atlantic borders
-        for (int i = 0; i < m; i++) {
-            aq.offer(new int[]{i, n - 1});
-            atl[i][n - 1] = true;
-        }
-        for (int j = 0; j < n; j++) {
-            aq.offer(new int[]{m - 1, j});
-            atl[m - 1][j] = true;
-        }
-
-        bfs(heights, pq, pac);
-        bfs(heights, aq, atl);
+        // Atlantic (bottom row + right column)
+        for (int i = 0; i < n; i++) dfs(i, m - 1, atlantic);
+        for (int j = 0; j < m; j++) dfs(n - 1, j, atlantic);
 
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pac[i][j] && atl[i][j])
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] == 1 && atlantic[i][j] == 1) {
                     res.add(Arrays.asList(i, j));
+                }
             }
         }
 
         return res;
     }
 
-    // BFS traversal
-    void bfs(int[][] h, Queue<int[]> q, boolean[][] vis) {
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
+    void dfs(int x, int y, int[][] vis) {
 
-            for (int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
+        vis[x][y] = 1;
 
-                if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
-                if (vis[nr][nc]) continue;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
-                if (h[nr][nc] >= h[r][c]) { // reverse condition
-                    vis[nr][nc] = true;
-                    q.offer(new int[]{nr, nc});
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+
+                // reverse condition
+                if (vis[nx][ny] == 0 && grid[nx][ny] >= grid[x][y]) {
+                    dfs(nx, ny, vis);
                 }
             }
         }
