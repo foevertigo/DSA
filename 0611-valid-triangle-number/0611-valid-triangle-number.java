95007aea508caa223874fe50c78e00d9148ebdc0
Aleena Burney <aleenaburney17@gmail.com>
2026-04-02 01:02:51 +0530

    Time: 30 ms (22.37%), Space: 45.8 MB (55.11%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int k=nums.length - 1; k>=2;k--) {
+            int i =0;
+            int j = k - 1;
+            while(i<j) {
+                if(nums[k]<nums[i]+nums[j]) {
+                     count += (j-i);
+                     j--;
+                } else {
+                    i++;
+                }
+                 
+            }
+        }
+        return count;
+    }
+}
