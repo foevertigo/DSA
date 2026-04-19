49a843c9a967b8c2561665cf9e6951c344f4f9f7
Aleena Burney <aleenaburney17@gmail.com>
2026-04-19 10:30:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,9 +1,25 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int idx = word.indexOf(ch);
+        char[] arr = word.toCharArray();
+        int idx = -1;
+
+        for(int i=0; i<arr.length;i++) {
+            if(arr[i]==ch) {
+                idx = i;
+                break;
+            }
+        }
+
         if(idx==-1) return word;
-        StringBuilder sb = new StringBuilder(word.substring(0,idx+1));
-        sb.reverse();
-        return sb.toString() + word.substring(idx+1);
+        int l=0, r=idx;
+        while(l<r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+
+        return new String(arr);
     }
 }
