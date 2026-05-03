2cae7de73993f8c2670cd5b560945a563a9c541d
Aleena Burney <aleenaburney17@gmail.com>
2026-05-03 17:46:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,16 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        List<int[]> result = new ArrayList<>();
-        int zero = 0, one = 0, two = 0;
-        for(int num:nums) {
-            if(num==0) zero++;
-            else if(num==1) one++;
-            else two++;
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid<=high) {
+            if(nums[mid]==0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1) {
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
         }
-        int i=0;
-        while(zero-->0) nums[i++]=0;
-        while(one-->0) nums[i++]=1;
-        while(two-->0) nums[i++] = 2;
-        
+            
     }
 }
